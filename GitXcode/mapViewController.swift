620fed0b8d740978ab6cf8736cc31fa2e0ad0b50
Abdulkarim Ali <abdulkarimali@macbook-pro-alkhas-b-sona.local>
2017-03-12 11:42:39 +0300

    mada fake

--- GitXcode/mapViewController.swift
+++ GitXcode/mapViewController.swift
@@ -12,6 +12,7 @@ class mapViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("hello madafaka")
 
         // Do any additional setup after loading the view.
     }
